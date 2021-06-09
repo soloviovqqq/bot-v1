e65fe1d1118199cc3d35030c12cf6c3125ec8108
d.soloviov <soloviovqqq@gmail.com>
2021-06-10 01:30:27 +0300

    telegram buy/sell notifications

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Notifications\BuyNotification;
+use App\Notifications\SellNotification;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Notification;
 
+/**
+ * Class Controller
+ * @package App\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public const CHAT_ID = -553452018;
+
+    /**
+     * @return JsonResponse
+     */
+    public function buy(): JsonResponse
+    {
+        Notification::route('telegram', self::CHAT_ID)
+            ->notify(new BuyNotification);
+
+        return response()->json();
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function sell(): JsonResponse
+    {
+        Notification::route('telegram', self::CHAT_ID)
+            ->notify(new SellNotification);
+
+        return response()->json();
+    }
 }
---
+++ app/Notifications/SellNotification.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Notifications;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Telegram\TelegramChannel;
+use NotificationChannels\Telegram\TelegramMessage;
+
+/**
+ * Class SellNotification
+ * @package App\Notifications
+ */
+class SellNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * @param $notifiable
+     * @return array
+     */
+    public function via($notifiable): array
+    {
+        return [TelegramChannel::class];
+    }
+
+    /**
+     * @param $notifiable
+     * @return TelegramMessage
+     */
+    public function toTelegram($notifiable): TelegramMessage
+    {
+        return TelegramMessage::create()
+            ->content('Super trend *SELL* alert. Alert time: ' . Carbon::now())
+            ->button('Open trading view', 'https://www.tradingview.com');
+    }
+}
